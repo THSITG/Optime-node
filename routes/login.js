9dc6aac0afc1477bb0463899ff4650969836b642
Liu Xiaoyi <circuitcoder0@gmail.com>
2015-02-23 03:32:20 +0800

    Finished login box

--- routes/login.js
+++ routes/login.js
@@ -8,6 +8,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   passport.authenticate('local', function(err,user,info) { 
+    res.setHeader("Content-Type","application/json");
     if(err) return next(err);
     if(!user) {
       res.write(JSON.stringify({
