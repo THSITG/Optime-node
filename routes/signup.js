88d6fa962b03228f3a54d91a81a8c31a4fa758de
Liu Xiaoyi <circuitcoder0@gmail.com>
2015-02-27 00:47:47 +0800

    Frontend for registration

--- routes/signup.js
+++ routes/signup.js
@@ -3,75 +3,83 @@ var router = express.Router();
 
 var nodemailer = require('nodemailer');
 var crypto = require('crypto');
-var md5 = crypto.createHash('md5');
 var salt = "whyimsohandsome";
 var confirm;
 var User = require('../schemas/user');
 
 router.post('/', function(req, res, next) {
-	token = req.body.password + req.body.email + salt;
-	md5.update(token);
-	var password = md5.digest('base64');
+  token = req.body.password + req.body.email + salt;
+  var md5 = crypto.createHash('md5');
+  md5.update(token);
+  var password = md5.digest('base64');
 
-  	var buf = crypto.randomBytes(32);
-  	var confirm = buf.toString('base64');
- 
-	var user = new User({
-		name: req.body.username,
-		email: req.body.email,
-		password: password,
-		active: false,
-		confirm: confirm
-	});
+  var buf = crypto.randomBytes(32);
+  var confirm = buf.toString('base64');
 
-	user.save(function (err, user) {
-		if (err) console.error(err);
-		console.log(user);
-	});
-		
-	var transporter = nodemailer.createTransport({
-		host: 'smtp.op-ti.me',
-		port: 25,
-		tls: {
-        	rejectUnauthorized:false
-    	},
-		auth: {
-			user: 'admin@op-ti.me',
-			pass: 'hehehe123'
-			}
-	});
-	console.log(confirm);
+  var user = new User({
+    name: req.body.username,
+    email: req.body.email,
+    password: password,
+    active: false,
+    confirm: confirm
+  });
 
-	var mailOptions = {
-		from: 'Optime <admin@op-ti.me>',
-		to: req.body.email,
-		subject: 'Welcome to Optime',
-		html: '<p>click <a href="https://op-ti.me/signup/confirm?code=' + confirm + '">here</a> to confirm your email</p>'
-	};
+  user.save(function (err, user) {
+    if (err) console.error(err);
+    console.log(user);
+  });
 
-	transporter.sendMail(mailOptions, function(error, info){
-		if(error){
-			console.log(error);
-		}else{
-			console.log('Message sent: ' + info.response);
-		}
-	});
+  var transporter = nodemailer.createTransport({
+    host: 'smtp.op-ti.me',
+    port: 25,
+    tls: {
+      rejectUnauthorized:false
+    },
+    auth: {
+      user: 'admin@op-ti.me',
+      pass: 'hehehe123'
+    }
+  });
+  console.log(confirm);
+
+  var mailOptions = {
+    from: 'Optime <admin@op-ti.me>',
+    to: req.body.email,
+    subject: 'Welcome to Optime',
+    html: '<p>click <a href="https://op-ti.me/signup/confirm?code=' + confirm + '">here</a> to confirm your email</p>'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log(error);
+      res.send({
+        success: false,
+        message: "Cannot send the email"
+      });
+    }else{
+      console.log('Message sent: ' + info.response);
+      res.send({
+        success: true,
+        target: req.body.email
+      });
+    }
+  });
 });
 
 router.get('/confirm', function(req, res, next) {
-	code = req.query.code;
-	var query = User.findOne({confirm: code});
-	query.select('name email active');
-	query.exec(function(err, user) {
-		if(err) console.console.log(err);
-		if (!user) {
-			return done(null, false, { message: 'Activation failed' });
-		}
-		else {
-			user.active = true;
-			return done(null, false, { message: 'Activation Success' });
-		}
-	});
+  code = req.query.code;
+  var query = User.findOne({confirm: code});
+  query.select('name email active');
+  query.exec(function(err, user) {
+    if(err) console.console.log(err);
+    if (!user) {
+      return done(null, false, { message: 'Activation failed' });
+    }
+    else {
+      user.active = true;
+      return done(null, false, { message: 'Activation Success' });
+    }
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,14 @@ var User = require('../schemas/user');
 var Board = require('../schemas/board');
 var Task = require('../schemas/task');
 
+function generateBoardID() {
+  return 1;
+}
+
+function generateTaskID() {
+  return 1;
+}
+
 // 获取主页面
 router.get('/:uid', function(req, res, next) {
   var result = {};
@@ -88,35 +96,41 @@ router.get('/:uid/boards/:bid', function(req, res, next) {
 
 // 创建 board
 router.post('/:uid/boards', function(req, res, next) {
-		var board = new Board({
-      name: req.body.name,
-      tasks: [],
-      members: [{
-        name: req.params.uid,
-        admin: true
-      }]
-    });
+  var generatedID=generateBoardID();
+  var board = new Board({
+    id: generatedID,
+    name: req.body.name,
+    tasks: [],
+    members: [{
+      name: req.params.uid,
+      admin: true
+    }]
+  });
 
-    board.save(function(err, board) {
-      if(err) console.log(err);
-      console.log(board);
-    });
+  board.save(function(err, board) {
+    if(err) console.log(err);
+    console.log(board);
+  });
 
-    User.findOne({name: req.params.uid}, function(err, user) {
-      if(err) console.log(err);
-      if(!user) {
-        res.send({error: 'no user'});
-      } else {
-        user.boards.push({id: });
-      }
-    });
-    res.send();
+  User.findOne({name: req.params.uid}, function(err, user) {
+    if(err) console.log(err);
+    if(!user) {
+      res.send({error: 'no user'});
+    } else {
+      user.boards.push({id: generatedID});
+      res.send({
+        error: false,
+        bid: generatedID
+      });
+    }
+  });
+  res.send();
 });
 
 // 删除 board
 router.delete('/:uid/boards/:bid', function(req, res, next) {
   User.findOne({name: req.params.uid}, function(err, user) {
-    user.boards.pull({id: });
+    user.boards.pull({id: req.params.bid});
   });
   Board.remove({id: req.params.bid}, function(err) {
     if(err) console.log(err);
@@ -125,45 +139,47 @@ router.delete('/:uid/boards/:bid', function(req, res, next) {
 
 // 创建新 task
 router.post('/:uid/boards/:bid/tasks', function(req, res, next) {
-	var task = new Task ({
-      name: req.body.name,
-      description: req.body.description,
-      importance: req.body.importance,
-      finished: false
-    });
+  var generatedID = generateTaskID();
+  var task = new Task ({
+    id: generatedID,
+    name: req.body.name,
+    description: req.body.description,
+    importance: req.body.importance,
+    finished: false
+  });
 
-    task.save(function(err, task) {
-      if(err) console.log(err);
-      console.log(task);
-    });
+  task.save(function(err, task) {
+    if(err) console.log(err);
+    console.log(task);
+  });
 
-    Board.findOne({id: req.params.bid}, function(err, board) {
-      if(err) console.log(err);
-      if(!board) {
-        res.send({error: 'no board'});
-      } else {
-        board.tasks.push({id: });
-        res.send();
-      }
-    });
+  Board.findOne({id: req.params.bid}, function(err, board) {
+    if(err) console.log(err);
+    if(!board) {
+      res.send({error: 'no board'});
+    } else {
+      board.tasks.push({id: generatedID});
+      res.send();
+    }
+  });
 });
 
 // 更新 task
 router.put('/:uid/boards/:bid/tasks/:tid', function(req, res, next) {
-		Task.update({id: req.params.tid}, {
-      name: req.body.name,
-      description: req.body.des,
-      finished: req.body.finished,
-      importance: req.body.importance
-    }, function(err) {
-      if(err) console.log(err);
-    });
+  Task.update({id: req.params.tid}, {
+    name: req.body.name,
+    description: req.body.des,
+    finished: req.body.finished,
+    importance: req.body.importance
+  }, function(err) {
+    if(err) console.log(err);
+  });
 });
 
 // 删除 task
 router.delete('/:uid/boards/:bid/tasks/:tid', function(req, res, next) {
 	Board.findOne({name: req.params.bid}, function(err, board) {
-    board.tasks.pull({id: });
+    board.tasks.pull({id: req.params.tid});
   });
   Task.remove({id: req.params.tid}, function(err) {
     if(err) console.log(err);
